2cd643d9f8914a7f672f8a3d946f88c72abc6cd9
macie <macie@DESKTOP-1EO416O>
2019-11-24 00:26:05 +0100

    Created prototype UI

---
+++ app/src/app/pages/financial-manager/financial-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-manager',
+  templateUrl: './financial-manager.component.html',
+  styleUrls: ['./financial-manager.component.scss']
+})
+export class FinancialManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
